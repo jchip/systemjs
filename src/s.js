e02296b641201f750386e0beffbc563b4f54a04c
Joel Chen <joel123@gmail.com>
2026-08-16 00:42:01 -0700

    SJS-1: remove converted JS sources

--- src/s.js
+++
@@ -1,6 +0,0 @@
-import './features/script-load.js';
-import './features/fetch-load.js';
-import './features/resolve.js';
-import './features/import-maps.js';
-import './features/depcache.js';
-import './features/worker-load.js';
--- src/system.js
+++
@@ -1,9 +0,0 @@
-import './features/script-load.js';
-import './features/fetch-load.js';
-import './features/resolve.js';
-import './features/import-maps.js';
-import './features/depcache.js';
-import './features/worker-load.js';
-import './extras/global.js';
-import './extras/module-types.js';
-import './features/registry.js';
